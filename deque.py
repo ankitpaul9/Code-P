2bbb8b832b2a5eb4d9dce2890436cdc6700226a9
Ankit <ankit.ext90@gmail.com>
2018-09-17 13:00:47 -0700

    Set deque

---
+++ deque.py
@@ -0,0 +1,21 @@
+class Deque:
+    def __init__(self):
+        pass
+
+    def isEmpty(self):
+        pass
+
+    def addFront(self, item):
+        pass
+
+    def addRear(self, item):
+        pass
+
+    def removeFront(self):
+        pass
+
+    def removeRear(self):
+        pass
+
+    def size(self):
+        pass
